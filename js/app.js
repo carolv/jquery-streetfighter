92bc49af17aef2a6d5b8b7a59b2f46cf032ded24
carolv <carol@oakcity.io>
2014-12-04 16:10:30 -0500

    adding in keydown handler

--- js/app.js
+++ js/app.js
@@ -1,10 +1,53 @@
 $(document).ready(function () {
     $('.ryu').mouseenter(function() {
         $('.ryu-still').hide();
+        $('.ryu-cool').hide();
         $('.ryu-ready').show();
     })
     .mouseleave(function() {
         $('.ryu-ready').hide();
         $('.ryu-still').show();
+        $('.ryu-cool').hide();
+    })
+    .mousedown(function() {
+        playHadouken();
+        $('.ryu-ready').hide();
+        $('.ryu-cool').hide();
+        $('.ryu-throwing').show();
+        $('.hadouken').finish().show()
+        .animate(
+            {'left': '300px'},
+            500,
+            function() {
+                $(this).hide();
+                $(this).css('left', '-212px');
+            }
+        );
+    })
+    .mouseup(function() {
+        //ryu goes back to ready
+        $('.ryu-throwing').hide();
+        $('.ryu-cool').hide();
+        $('.ryu-ready').show();
+    });
+    $(document).keydown(function(event) {
+        if(event.keyCode == 88){
+            $('.ryu-still').hide();
+            $('.ryu-ready').hide();
+            $('.ryu-throwing').hide();
+            $('.ryu-cool').show();
+        }
+    })
+    .keyup(function(event) {
+        if(event.keyCode == 88) {
+            $('.ryu-cool').hide();
+            $('.ryu-still').show();
+        }
     });
 });
+
+function playHadouken () {
+    $('#hadouken-sound')[0].volume = 0.5;
+    $('#hadouken-sound')[0].load();
+    $('#hadouken-sound')[0].play();
+}
